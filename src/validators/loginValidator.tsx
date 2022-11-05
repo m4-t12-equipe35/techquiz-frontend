e0ff7ebf6f48c5d4bad31d7a17a044c86b62853d
MatheusMendesSB <matheusmendessb@gmail.com>
2022-11-05 18:31:15 -0300

    feat: regex for password register schema

--- src/validators/loginValidator.tsx
+++ src/validators/loginValidator.tsx
@@ -1,8 +1,11 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
-    email: yup.string().required('Email é obrigatório'),
-    password: yup.string().required('Senha é obrigatória')
-})
+  email: yup
+    .string()
+    .email("Digite um e-mail válido!")
+    .required("Email obrigatório!"),
+  password: yup.string().required("Senha obrigatória!"),
+});
 
-export default loginSchema
+export default loginSchema;
--- src/validators/registerValidator.tsx
+++ src/validators/registerValidator.tsx
@@ -1,16 +1,26 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 const registerSchema = yup.object().shape({
-    name: yup.string().required("Nome é obrigatório"),
-    email: yup.string().required("Email é obrigatório").email("Email inválido"),
-    password: yup
-      .string()
-      .required("Senha é obrigatória"),
-    confirmPassword: yup
-      .string()
-      .required("Confirmação de senha é obrigatória")
-      .oneOf([yup.ref("password")], "Confirmação de senha está incorreta"),
-    stack: yup.string().required('Seleção de stack é obrigatória')
-})
+  name: yup.string().required("Nome obrigatório!"),
+  email: yup
+    .string()
+    .required("Email obrigatório!")
+    .email("Digite um e-mail válido!"),
+  password: yup
+    .string()
+    .required("Senha obrigatória!")
+    .matches(
+      /(^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*_-])).{8,}$/,
+      "Formato: 8 caracteres, 1 letra maiúscula, 1 minúscula, 1 número e 1 caracter especial!"
+    ),
+  confirmPassword: yup
+    .string()
+    .required("Você precisa confirmar sua senha!")
+    .oneOf(
+      [yup.ref("password")],
+      "Você precisa digitar a mesma senha do campo anterior!"
+    ),
+  stack: yup.string().required("É necessário selecionar uma stack!"),
+});
 
-export default registerSchema
+export default registerSchema;
