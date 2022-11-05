8e5d85ab79a338f8b271168a5c4df2243472bbb8
helena <heju1958@outlook.com>
2022-11-05 15:46:54 -0300

    feat: estrutura inicial da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Homepage from "./components/Home/home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
