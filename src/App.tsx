99213ac87b0b62ba4a0d5ac050fe1534651c8cef
MatheusMendesSB <matheusmendessb@gmail.com>
2022-11-05 18:12:49 -0300

    feat: global style imported on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import GlobalStyle from "../src/styles/global";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
