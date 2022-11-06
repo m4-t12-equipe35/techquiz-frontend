bd7db6fa068c1df4633eccf72c4dee963ada39b9
helena <heju1958@outlook.com>
2022-11-06 14:17:13 -0300

    feat: filtrar por tech

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react";
+import api from "../../services";
+interface ITech {
+  id: string;
+  name: string;
+  stack: string;
+}
+
 const Homepage = () => {
+  const [tech, setTech] = useState<ITech[]>([]);
+
+  useEffect(() => {
+    api
+      .get("/techs")
+      .then((res) => {
+        setTech(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const [search, setSearch] = useState("");
+
+  const showTechs = () => {
+    if (tech) {
+      const input = search.toLocaleLowerCase();
+      const productInput = tech.filter((elem) =>
+        elem.name.toLowerCase().includes(input)
+      );
+      setTech(productInput);
+    }
+  };
+
   return (
     <>
       <div>
@@ -6,16 +37,25 @@ const Homepage = () => {
         <div>
           <p>Teste seus conhecimentos sobre as tecnologias que você gosta</p>
           <input
-            onChange={(event) => event.target.value}
+            onChange={(event) => setSearch(event.target.value)}
             type="text"
             placeholder="Pesquise uma tecnologia"
           />
-          <button type="submit">Buscar</button>
+          <button
+            type="submit"
+            onClick={() => {
+              showTechs();
+            }}
+          >
+            Buscar
+          </button>
         </div>
         <img src="" alt="image2" />
       </div>
       <ul>
-        <li></li>
+        {tech.map((elem) => {
+          return <li key={elem.id}>{elem.name}</li>;
+        })}
       </ul>
       <div>
         <p>Teste seus conhecimentos sobre as tecnologias que você gosta</p>
