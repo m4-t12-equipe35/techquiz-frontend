76fc0d21e4f796e73072a89833947f75816ac163
helena <heju1958@outlook.com>
2022-11-08 21:06:48 -0300

    bugfix: style footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: var(--grey-light);
-    color: var(--grey-text);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--grey-light);
+  color: var(--grey-text);
 
-    width: 100%;
-    height: 60px;
+  width: 100%;
+  height: 60px;
 
-    p{
-        font-size: 10px;
-        font-weight: 300;
+  p {
+    font-size: 10px;
+    font-weight: 300;
+  }
+
+  @media (min-width: 768px) {
+    margin-top: 6.5rem;
+    height: 40px;
+
+    p {
+      font-size: 14px;
     }
-
-    @media (min-width: 768px) {
-      
-        height: 40px;
-        
-        p{
-            font-size: 14px;
-        }
-    }
-`
+  }
+`;
